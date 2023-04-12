6c596cc72d56a09d076a65c46c10d05f0a0476cb
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-12 13:00:26 +0200

    Refactored: changed return type to Promise<IVerifyToken> for improved type safety and removed unnecessary import

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,21 +1,28 @@
 import { Request } from "express";
-import Jwt from "jsonwebtoken";
+import Jwt, { VerifyErrors } from "jsonwebtoken";
+import { IVerifyToken } from "../interface/interface";
 
 const jwtSecret = process.env.JWT_SECRET;
 
- const verifyToken = (req: Request) => {
+const verifyToken = (req: Request): Promise<IVerifyToken> => {
   if (!jwtSecret) {
     throw new Error("Unable to verify JWT secret");
   }
-  return new Promise((resolve, reject) => {
-    Jwt.verify(req.body.token, jwtSecret, {}, (error, decodedToken) => {
-      if (error) {
-       return reject(error);
-      }
 
-      return resolve(decodedToken);
-    });
+  return new Promise((resolve, reject) => {
+    Jwt.verify(
+      req.body.token,
+      jwtSecret,
+      (error: VerifyErrors | null, decodedToken?: object) => {
+        if (error || !decodedToken) {
+          return reject(error);
+        }
+
+        const { id } = decodedToken as IVerifyToken;
+        return resolve({ id });
+      }
+    );
   });
 };
 
-export default verifyToken
+export default verifyToken;
