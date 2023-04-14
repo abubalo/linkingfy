c2a221ffafd82f93e69bb895549f8e27395cb6c1
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-14 22:41:19 +0200

    Added tailwind and postcss for styling

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{tsx, ts}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        gradient: "linear-gradient(113.28deg, #FF8A00 4.91%, #FF2B68 94.62%)",
+      },
+    },
+   
+  },
+  plugins: [],
+};
