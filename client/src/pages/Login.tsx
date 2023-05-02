f17900f90433f9a699b1712985b924f5bbe916b4
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-02 18:43:38 +0200

    style: Added subtle animation

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate, redirect } from "react-router-dom";
 import { useFormik, FormikValues } from "formik";
 import * as Yup from "yup";
-import axios from "axios"
+import axios from "axios";
+import { AnimatePresence, motion } from "framer-motion";
 
 type Props = {};
 
 const Login = (props: Props) => {
-  const [emailError, setEmailError] = useState("")
-  const [emailPassword, setEmailPassword] = useState("")
+  const [userError, setUserError] = useState<string>("");
+  const [emailPassword, setEmailPassword] = useState<string>("");
+  const [redirect, setRedirect] = useState<boolean>(false);
 
   const formik = useFormik({
     initialValues: {
@@ -18,82 +20,130 @@ const Login = (props: Props) => {
 
     validationSchema: Yup.object({
       email: Yup.string().email("Invliad email").required("Email is required"),
-      password: Yup.string()
-        .min(6, "Password must be more than six characters")
-        .required("Please provide password"),
+      password: Yup.string().required("Please provide password"),
     }),
 
     onSubmit: async (values: FormikValues) => {
       try {
-          const response = await axios.post("")
-      } catch (error) {
-        
+        await axios.post("/user/login", values);
+        // Redirect user if there are no validation errors
+        setRedirect(true);
+      } catch (error: any) {
+        // Handle any errors that occur during the validation process
+        if (error.response && error.response.status === 404) {
+          setUserError("User doest not exists");
+        }
+        if (error.response && error.response.status === 401) {
+          setUserError("Invalid email or password");
+        }
+        console.log(error.message);
       }
     },
   });
+
+  if (redirect) {
+    return <Navigate to={"/dashboard"} />;
+  }
   return (
-    <main className="flex flex-col items-center justify-center w-full h-screen bg-gradient">
-      <form
-        action=""
-        className="w-[90%] mx-auto space-y-3 bg-white lg:w-1/3 p-4 rounded-md"
-      >
-        <h1 className="text-2xl font-semibold text-center">Welcome back!</h1>
-        <div className="flex flex-col space-y-2">
-          <label htmlFor="">Email:</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="johndoe@gmail.com"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            className={`${formik.errors.email ? "border-red-500" : "border-green-600"}`}
-          />
-          <small
-            className={`text-red-500 ${
-              formik.touched.email && formik.errors.email
-                ? "visible"
-                : "invisble"
-            }`}
-          >
-            {formik.errors.email}
-          </small>
-        </div>
-        <div className="flex flex-col space-y-2">
-          <label htmlFor="">Password:</label>
-          <input
-            type="password"
-            name="password"
-            className={`${formik.errors.password ? "border-red-500" : "border-green-600"}`}
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          <small
-            className={`text-red-500 ${
-              formik.touched.password && formik.errors.password
-                ? "visible"
-                : "invisble"
-            }`}
-          >
-            {formik.errors.password}
-          </small>
-        </div>
-        <div>
-          <button type="submit" className="">
-            Login
-          </button>
-        </div>
-        <div>
-          <small>
-            Dont have account?{" "}
-            <Link className="text-blue-500" to={"/signup"}>
-              Signup
-            </Link>{" "}
-          </small>
-        </div>
-      </form>
-    </main>
+    <AnimatePresence>
+      <main className="flex flex-col items-center justify-center w-full h-screen bg-gradient">
+        <motion.form
+          initial={{ scale: 0.98 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5, delay: 0.25, ease: "linear" }}
+          onSubmit={formik.handleSubmit}
+          action=""
+          className="w-[90%] mx-auto space-y-3 bg-white lg:w-1/3 p-4 rounded-md transition-all duration-75 ease-linear"
+        >
+          <>
+            <h1 className="text-2xl font-semibold text-center">
+              Welcome back!
+            </h1>
+            {/* Display server side errors that is not tied to any input */}
+            <div>
+              {userError != "" && (
+                <motion.small
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.25 }}
+                  className="text-red-500 font-semibold"
+                >
+                  {userError}
+                </motion.small>
+              )}
+            </div>
+
+            <div className="flex flex-col space-y-2">
+              <label htmlFor="">Email:</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="johndoe@gmail.com"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                className={`${
+                  formik.errors.email ? "border-red-500" : "border-green-600"
+                }`}
+              />
+              <motion.small
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.25 }}
+                exit={{ opacity: 0 }}
+                className={`text-red-500 font-semibold ${
+                  formik.touched.email && formik.errors.email
+                    ? "visible"
+                    : "invisble"
+                }`}
+              >
+                {formik.errors.email}
+              </motion.small>
+            </div>
+            <div className="flex flex-col space-y-2">
+              <label htmlFor="">Password:</label>
+              <input
+                type="password"
+                name="password"
+                className={`${
+                  formik.errors.password ? "border-red-500" : "border-green-600"
+                }`}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              <motion.small
+                key={3}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.25 }}
+                exit={{ opacity: 0 }}
+                className={`text-red-500 font-semibold ${
+                  formik.touched.password && formik.errors.password
+                    ? "visible"
+                    : "invisble"
+                }`}
+              >
+                {formik.errors.password}
+              </motion.small>
+            </div>
+            <div>
+              <button type="submit" className="">
+                Login
+              </button>
+            </div>
+            <div>
+              <small>
+                Don't have account yet?{" "}
+                <Link className="text-blue-500" to={"/signup"}>
+                  Signup
+                </Link>{" "}
+              </small>
+            </div>
+          </>
+        </motion.form>
+      </main>
+    </AnimatePresence>
   );
 };
 
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Navigate, redirect } from "react-router-dom";
 import { FormikValues, useFormik, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
+import { AnimatePresence, motion } from "framer-motion";
 
-type Props = {};
 
-const Signup = (props: Props) => {
+
+const Signup = () => {
+  const [redirect, setRediect] = useState<boolean>(false)
   const formik = useFormik({
     initialValues: {
       fullname: "",
@@ -15,105 +18,136 @@ const Signup = (props: Props) => {
 
     validationSchema: Yup.object({
       fullname: Yup.string().required("Full name is required"),
-      email: Yup.string()
-        .email("Invliad email")
-        .required("Email is required"),
+      email: Yup.string().email("Invliad email").required("Email is required"),
       password: Yup.string()
         .min(6, "Password must be more than six characters")
         .required("Please provide password"),
     }),
 
-    onSubmit: (values: FormikValues) => {},
+    onSubmit: async (
+      values: FormikValues,
+      { setSubmitting, setErrors, setStatus }
+    ) => {
+      try {
+        await axios.post("/user/register", values);
+        // Redirect to dashbord if successfully signed up
+       setRediect(true)
+      } catch (error: any) {
+        // Check if the user alrady exists
+        if (error.response && error.response.status === 409) {
+          setErrors({
+            email: "User already exists",
+          });
+        }
+        console.log(error);
+      }
+    },
   });
-  console.log(formik.values);
-  console.log(formik.errors);
+
+  if(redirect){
+    return <Navigate to="/dashboard"/>;
+  }
+  console.log(formik.errors.email);
   return (
-    <main className="flex flex-col items-center justify-center w-full h-screen bg-gradient">
-      <form
-        onSubmit={formik.handleSubmit}
-        action=""
-        className="w-[90%] mx-auto space-y-3 bg-white lg:w-1/3 p-4 rounded-md"
+    <AnimatePresence>
+      <main
+        className="flex flex-col items-center justify-center w-full h-screen bg-gradient"
       >
-        <h1 className="text-2xl font-semibold text-center">
-          Sign up for Short.ly
-        </h1>
-        <div className="flex flex-col space-y-2">
-          <label htmlFor="">Fullname:</label>
-          <input
-            type="text"
-            name="fullname"
-            placeholder="John Doe"
-            value={formik.values.fullname}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            className={`${formik.errors.fullname ? "border-red-500" : "border-green-600"}`}
-          />
-          <small
-            className={`text-red-500 ${
-              formik.touched.fullname && formik.errors.fullname
-                ? "visible"
-                : "invisble"
-            }`}
-          >
-            {formik.errors.fullname}
-          </small>
-        </div>
-        <div className="flex flex-col space-y-2">
-          <label htmlFor="">Email:</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="johndoe@gmail.com"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            className={`${formik.errors.email ? "border-red-500" : "border-green-600"}`}
-          />
-          <small
-            className={`text-red-500 ${
-              formik.touched.email && formik.errors.email
-                ? "visible"
-                : "invisble"
-            }`}
-          >
-            {formik.errors.email}
-          </small>
-        </div>
-        <div className="flex flex-col space-y-2">
-          <label htmlFor="">Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            className={`${formik.errors.password ? "border-red-500" : "border-green-600"}`}
-          />
-          <small
-            className={`text-red-500 ${
-              formik.touched.password && formik.errors.password
-                ? "visible"
-                : "invisble"
-            }`}
-          >
-            {formik.errors.password}
-          </small>
-        </div>
-        <div>
-          <button type="submit" className="">
-            Sign up
-          </button>
-        </div>
-        <div>
-          <small>
-            Already a user?{" "}
-            <Link className="text-blue-500" to={"/login"}>
-              Login
-            </Link>{" "}
-          </small>
-        </div>
-      </form>
-    </main>
+        <motion.form
+          initial={{scale:0.98}}
+          animate={{scale: 1}}
+          transition={{duration: 0.5, delay: 0.25, ease: "linear"}}
+          onSubmit={formik.handleSubmit}
+          action=""
+          className="w-[90%] mx-auto space-y-3 bg-white lg:w-1/3 p-4 rounded-md"
+        >
+          <h1 className="text-2xl font-semibold text-center">
+            Sign up for Short.ly
+          </h1>
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="">Fullname:</label>
+            <input
+              type="text"
+              name="fullname"
+              placeholder="John Doe"
+              value={formik.values.fullname}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className={`${
+                formik.errors.fullname ? "border-red-500" : "border-green-600"
+              }`}
+            />
+            <small
+              className={`text-red-500 ${
+                formik.touched.fullname && formik.errors.fullname
+                  ? "visible"
+                  : "invisble"
+              }`}
+            >
+              {formik.errors.fullname}
+            </small>
+          </div>
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="">Email:</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="johndoe@gmail.com"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className={`${
+                formik.errors.email ? "border-red-500" : "border-green-600"
+              }`}
+            />
+            <small
+              className={`text-red-500 ${
+                formik.touched.email && formik.errors.email
+                  ? "visible"
+                  : "invisble"
+              }`}
+            >
+              {formik.errors.email}
+            </small>
+          </div>
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="">Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className={`${
+                formik.errors.password ? "border-red-500" : "border-green-600"
+              }`}
+            />
+            <small
+              className={`text-red-500 ${
+                formik.touched.password && formik.errors.password
+                  ? "visible"
+                  : "invisble"
+              }`}
+            >
+              {formik.errors.password}
+            </small>
+          </div>
+          <div>
+            <button type="submit" className="">
+              Sign up
+            </button>
+          </div>
+          <div>
+            <small>
+              Already a user?{" "}
+              <Link className="text-blue-500" to={"/login"}>
+                Login
+              </Link>{" "}
+            </small>
+          </div>
+        </motion.form>
+      </main>
+    </AnimatePresence>
   );
 };
 
