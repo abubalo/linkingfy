99966f0af982e2ed7a480fd22924965265a5e42d
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-02 08:21:19 +0200

    Fixed jwt import error

--- backend/src/controllers/qrcodeControllers.ts
+++ backend/src/controllers/qrcodeControllers.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from "express"
 import User from "../models/User"
-import { Jwt } from "jsonwebtoken"
+import Jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
 export const generate = (req:Request, res:Response)=>{
